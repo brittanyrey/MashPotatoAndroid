8f0a68f4d7a81165a8b5b47cb52fbb4590b4d7d8
Jessica Chen <jlchen@email.wm.edu>
2013-12-16 02:37:16 -0500

    adding things

--- MashPotato/src/edu/wm/mashpotato/HomeScreenActivity.java
+++ MashPotato/src/edu/wm/mashpotato/HomeScreenActivity.java
@@ -31,12 +31,16 @@ import edu.wm.mashpotato.web.WebTask;
 
 import android.app.Activity;
 import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.IntentFilter.MalformedMimeTypeException;
 import android.graphics.Color;
 import android.graphics.PorterDuffColorFilter;
 import android.graphics.PorterDuff;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.nfc.NdefMessage;
 import android.nfc.NdefRecord;
 import android.nfc.NfcAdapter;
@@ -62,11 +66,17 @@ import android.widget.Toast;
 import android.widget.ViewFlipper;
 
 public class HomeScreenActivity extends Activity
-	 implements CreateNdefMessageCallback, OnNdefPushCompleteCallback {
+	 implements CreateNdefMessageCallback, OnNdefPushCompleteCallback, LocationListener {
 	 NfcAdapter mNfcAdapter;
 	// NfcManager mNfcManager;
 
 	private static final int MESSAGE_SENT = 1;
+	private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 10; // 10 meters
+	 
+    // The minimum time between updates in milliseconds
+    private static final long MIN_TIME_BW_UPDATES = 1000 * 60 * 1; // 1 minute
+    LocationManager mlocManager;
+    private boolean canGetLocation;
 
 	private static final String TAG = "HomeScreenActivity";
 
@@ -138,7 +148,10 @@ public class HomeScreenActivity extends Activity
 		stats.setBackgroundResource(R.drawable.info);
 		stats.setColorFilter(new PorterDuffColorFilter(Color.GREEN, PorterDuff.Mode.MULTIPLY));
 		logout.setBackgroundResource(R.drawable.power);
-		
+		 mlocManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+
+         LocationListener mlocListener = this;
+         mlocManager.requestLocationUpdates( LocationManager.GPS_PROVIDER, MIN_TIME_BW_UPDATES, 0, mlocListener);
 		  mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
 		  if(player.isHasString()){
 			  mNfcAdapter.setNdefPushMessageCallback(this, this);
@@ -222,6 +235,7 @@ public class HomeScreenActivity extends Activity
 				}
 			}
 		});
+		mHandler.postDelayed(dynamicPoll, 30000);
 	}
 
 	// TODO DOES NOT WORK
@@ -523,4 +537,114 @@ public class HomeScreenActivity extends Activity
 			} 
 		}
 	}
+    
+    @Override
+    public void onLocationChanged(Location loc)
+    {
+        player.lat = loc.getLatitude();
+        player.lng = loc.getLongitude();
+    }
+
+    @Override
+    public void onProviderDisabled(String provider)
+    {
+      Toast.makeText( getApplicationContext(), "Gps Disabled", Toast.LENGTH_SHORT ).show();
+    }
+
+    @Override
+    public void onProviderEnabled(String provider)
+    {
+      Toast.makeText( getApplicationContext(), "Gps Enabled", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras)
+    {
+
+    }
+    
+    public Location getLocation() {
+        Location location = null;
+        // Log.e(TAG, "Get Location");
+         try {
+
+            // getting GPS status
+            boolean isGPSEnabled = mlocManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+            // getting network status
+            boolean isNetworkEnabled = mlocManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+            if (!isGPSEnabled && !isNetworkEnabled) {
+                // no network provider is enabled
+            } else {
+                this.canGetLocation = true;
+                // First get location from Network Provider
+                if (isNetworkEnabled) {
+                    mlocManager.requestLocationUpdates(
+                            LocationManager.NETWORK_PROVIDER,
+                            MIN_TIME_BW_UPDATES,
+                            MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+ // Log.d("Network", "Network");
+                    if (mlocManager != null) {
+                        location = mlocManager
+                                .getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                        if (location != null) {
+                            player.lat = location.getLatitude();
+                            player.lng = location.getLongitude();
+                        }
+                    }
+                }
+                // if GPS Enabled get lat/long using GPS Services
+                if (isGPSEnabled) {
+                    if (location == null) {
+                          mlocManager.requestLocationUpdates(
+                                LocationManager.GPS_PROVIDER,
+                                MIN_TIME_BW_UPDATES,
+                                MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+ // Log.d("GPS Enabled", "GPS Enabled");
+                        if (mlocManager != null) {
+                            location = mlocManager
+                                    .getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                            if (location != null) {
+                                player.lat = location.getLatitude();
+                                player.lng = location.getLongitude();
+                            }
+                        }
+                    }
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return location;
+    }
+    
+    Runnable dynamicPoll = new Runnable(){
+
+		@Override
+		public void run() {
+			List<NameValuePair> pairs = new ArrayList<NameValuePair>();
+			if(player.isHasString()){
+				pairs.add(new BasicNameValuePair(Constants.potatoId, player.getPotatoList().get(0)));
+				pairs.add(new BasicNameValuePair(Constants.temp, gameObj.getPotato().get(0).getTemp()+""));
+			}else{
+				pairs.add(new BasicNameValuePair(Constants.potatoId, ""));
+				pairs.add(new BasicNameValuePair(Constants.temp, 0+""));
+			}
+			pairs.add(new BasicNameValuePair(Constants.holder, username));
+			pairs.add(new BasicNameValuePair(Constants.score, player.getScore()+""));
+			
+			pairs.add(new BasicNameValuePair(Constants.lat, player.getLat()+""));
+			pairs.add(new BasicNameValuePair(Constants.lng, player.getLng()+""));
+			
+        	UserLoginTask task = new UserLoginTask(true, username, password, pairs, true, false);
+        	task.execute(Constants.updatePlayerInfo);
+        	long temp = (long)gameObj.getPotato().get(0).getTemp();
+        	long delay = gameObj.getMaxRoundTime()/ temp / 10;
+        	mHandler.postDelayed(this, delay);
+		}
+    	
+    };
 }
